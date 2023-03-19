45eaa9929a7fbcdc3242a2103567d638499efb98
Kuba Palinski <kpalinski99@gmail.com>
2023-03-20 06:30:40 +0800

    lifting state

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -19,15 +19,25 @@ const App = () => {
       objectID: 1,
     }
   ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      
-      <Search />
+      {/* // B */}
+      <Search onSearch={handleSearch}/>
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
@@ -52,21 +62,9 @@ const Item = (props) => (
   </li>
 );
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
+const Search = (props) => (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   );
-}
