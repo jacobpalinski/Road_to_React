c134cd4d482939e2ac395298d84065a495e9f5cb
Kuba Palinski <kpalinski99@gmail.com>
2023-03-23 06:31:50 +0800

    react component composition

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
   const stories = [
     {
-      title:'React',
+      title: 'React',
       url: 'https://reactjs.org/',
       author: 'Jordan Walke',
-      num_comments : 3,
+      num_comments: 3,
       points: 4,
       objectID: 0,
     },
@@ -17,44 +29,88 @@ const App = () => {
       num_comments: 2,
       points: 5,
       objectID: 1,
-    }
+    },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      {/* // B */}
-      <Search search={searchTerm} onSearch={handleSearch}/>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
       <List list={searchedStories} />
     </div>
   );
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  //A
+  const inputRef = React.useRef();
+
+  //C
+  React.useEffect((() => {
+    if (isFocused && inputRef.current) {
+      //D
+      inputRef.current.focus();
+    }
+  }))
+
 }
-export default App;
+(
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      autofocus = {isFocused}
+      onChange={onInputChange}
+    />
+  </>
+);
 
-const List = ({list}) => (
-    <ul>
-      {list.map((item) => (
-       <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-)
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
-const Item = ({item}) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href = {item.url}>{item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
@@ -62,12 +118,4 @@ const Item = ({item}) => (
   </li>
 );
 
-const Search = ({search,onSearch}) => (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" 
-      type="text"
-      value={search}  
-      onChange={onSearch} />
-    </div>
-  );
+export default App;
